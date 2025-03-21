e0e23b79ddfa9db330f7287b5b1edd2a5cd52ae2
raghava s.s.v <21CS01068@iitbbs.ac.in>
2025-03-21 17:33:58 +0530

    Add Post model and register to admin site

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Raghava',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'March 21, 2025'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'March 22, 2025'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
